25291828b7dfc6210eca88ca1c2942f370695b9d
xavierGarciaOrihuela <xavier.garcia.orihuela@estudiantat.upc.edu>
2023-03-27 18:40:06 +0200

    task #40: Html

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,51 @@
 class User < ApplicationRecord 
 
-    has_many :projects, dependent: :destroy
-    has_many :memberships, dependent: :destroy
-    has_many :member_projects, through: :memberships, source: :project
-  
-    def create_project(name)
-      projects.create(name: name)
-    end
-  
-    def join_project(project)
-      member_projects << project
-    end
-  
-    def leave_project(project)
-      member_projects.delete(project)
-    end
+  has_many :projects, dependent: :destroy
+  has_many :memberships, dependent: :destroy
+  has_many :member_projects, through: :memberships, source: :project
 
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :omniauthable, omniauth_providers: [:github]
+  has_many :issues, dependent: :destroy
 
-      def self.from_omniauth(access_token)
-        data = access_token.info
-        user = User.where(email: data['email']).first
-      
-        # Uncomment the section below if you want users to be created if they don't exist
-        unless user
-          user = User.create(
-                            email: data['email'],
-                            password: Devise.friendly_token[0,20]
-                            )
-        end
+  def create_project(name)
+    projects.create(name: name)
+  end
 
-        user
+  def join_project(project)
+    member_projects << project
+  end
 
+  def leave_project(project)
+    member_projects.delete(project)
+  end
+
+  def watch_issue(issue)
+    issues << issue
+  end
+
+  def unwatch_issue(issue)
+    issues.delete(issue)
+  end
+
+
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable, 
+       :omniauthable, omniauth_providers: [:github]
+
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+    
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+        user = User.create(
+                          email: data['email'],
+                          password: Devise.friendly_token[0,20]
+                          )
       end
+
+      user
+
+    end
 end
