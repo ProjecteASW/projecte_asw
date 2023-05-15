287aa5830271170d42b74b2d5c6cb92ff5657da6
xavierGarciaOrihuela <xavier.garcia.orihuela@estudiantat.upc.edu>
2023-05-15 13:34:58 +0200

    task #96 Veure Issue serializers

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,14 +1,37 @@
 class IssueSerializer < ActiveModel::Serializer
-    attributes :id, :subject, :description, :status, :created_by_user_id, :created_at, :issue_type, :severity, :priority, :limitDate, :blocked, :assigned_profile
+    attributes :id, :subject, :description, :status, :created_by_user_id, :created_by_user_username, :creation_date, :issue_type, :severity, :priority, :limitDate, :blocked, :assigned_profile_id, :assigned_profile_username, :watchers, :attachments, :activities
 
-    has_many :timeline_events, serializer: TimelineEventIssueSerializer
-    has_many :watched_issues, serializer: WatchedIssueIssueSerializer
+    has_many :comments, serializer: CommentSerializer
 
-    def assigned_profile
+    def assigned_profile_id
         object.assigned_to_id
+    end
+
+    def assigned_profile_username
+        User.find(object.assigned_to_id).username
     end
 
     def created_by_user_id
         object.user_id
+    end
+
+    def created_by_user_username
+        object.user.username
+    end
+
+    def creation_date
+        (object.created_at + Time.parse("02:00:00").seconds_since_midnight.seconds).strftime('%H:%M - %d/%m/%Y')
+    end
+    
+    def watchers
+        ActiveModel::Serializer::CollectionSerializer.new(object.watched_issue, serializer: WatchedIssueIssueSerializer)
+    end
+
+    def attachments
+        ActiveModel::Serializer::CollectionSerializer.new(object.files, serializer: AttachmentSerializer)
+    end
+
+    def activities
+        ActiveModel::Serializer::CollectionSerializer.new(object.timeline_events, serializer: TimelineEventIssueSerializer)
     end
 end
