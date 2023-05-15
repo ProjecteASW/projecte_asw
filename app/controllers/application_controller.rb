e7a68c9587587adff439887ba0109d07879b2de2
xavierGarciaOrihuela <xavier.garcia.orihuela@estudiantat.upc.edu>
2023-05-15 12:39:34 +0200

    task #92 Token usuaris + vistes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate
 
 protected
 
@@ -8,5 +9,23 @@ protected
   end
   def after_sign_in_path_for(resource)
     '/projects'
+  end
+
+  def authenticate
+    if request.format.html? 
+      authenticate_user!
+    end
+    if request.format.json?
+      if request.method != "GET"
+        if request.headers["API_KEY"].nil?
+          raise "API_KEY not provided."
+        else
+          user = User.find_by_api_key(api_key)
+          if user.nil?
+            raise "The API_KEY does not belong to any user."
+          end
+        end
+      end
+    end
   end
 end
